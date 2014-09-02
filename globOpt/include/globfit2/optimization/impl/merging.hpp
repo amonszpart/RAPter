1be2f15b874479d84997fe880bfd342f25a38236
Aron Monszpart <a.monszpart@cs.ucl.ac.uk>
2014-09-02 23:15:41 +0100

    less greedy reassignment in merging.hpp

--- globOpt/include/globfit2/optimization/impl/merging.hpp
+++ globOpt/include/globfit2/optimization/impl/merging.hpp
@@ -52,9 +52,10 @@ Merging::mergeCli( int argc, char** argv )
     } // ... parse params
 
     // read primitives
+    typedef std::vector<_PrimitiveT>    PatchT;
+    typedef std::map   < int, PatchT >  PrimitiveMapT;
     _PrimitiveContainerT prims;
-    typedef std::vector<_PrimitiveT> PatchT;
-    std::map< int, std::vector<_PrimitiveT> > prims_map;
+    PrimitiveMapT        prims_map;
     {
         io::readPrimitives<_PrimitiveT, PatchT>( prims, prims_path, &prims_map );
     } //...read primitives
@@ -99,21 +100,140 @@ Merging::mergeCli( int argc, char** argv )
             std::cerr << "[" << __func__ << "]: " << "point assigned to patch with id -1" << std::endl;
     }
 
+    //____________________________WORK____________________________
+
+    typedef GF2::MyPointPrimitiveDistanceFunctor _PointPrimitiveDistanceFunctor;
+
     // select unassigned points
-    for ( size_t i = 0; i != points.size(); ++i )
+    std::deque<int> orphan_pids;
     {
-        int gid = points[i].getTag( _PointPrimitiveT::GID );
-        if ( !prims_map[gid].size() )
+        for ( size_t pid = 0; pid != points.size(); ++pid )
         {
-            points[i].setTag( _PointPrimitiveT::GID, -2 );
+            int gid = points[pid].getTag( _PointPrimitiveT::GID );
+            if ( !prims_map[gid].size() ) // no primitives with this GID
+            {
+                points[pid].setTag   ( _PointPrimitiveT::GID, -2 );
+                orphan_pids.push_back( pid );
+            }
         }
     }
 
+    int change = 0, iteration = 0;
+    do
+    {
+        // reset re-assignment counter
+        change = 0;
+
+        // store possible primitives to assign points[pid] to
+        typedef std::pair<int,int> GidLid1;
+        std::map<int, std::vector<GidLid1> > adopter_gids; // < pid, [ <gid,lid1>, ... ] >
+        {
+            // iterate over unassigned points
+            std::deque<int>::const_iterator points_it_end = orphan_pids.end();
+            for ( std::deque<int>::const_iterator points_it = orphan_pids.begin(); points_it != points_it_end; ++points_it )
+            {
+                // store point id
+                const int pid = *points_it;
+
+                // for patches
+                typename PrimitiveMapT::const_iterator end_it = prims_map.end();
+                for ( typename PrimitiveMapT::const_iterator it = prims_map.begin(); it != end_it; ++it )
+                {
+                    // record primitive id in patch
+                    int lid1 = 0;
+                    // for primitives in patch
+                    typename PatchT::const_iterator end_it2 = it->second.end();
+                    for ( typename PatchT::const_iterator it2 = it->second.begin(); it2 != end_it2; ++it2, ++lid1 )
+                    {
+                        // distance from point to primitive
+                        _Scalar dist = _PointPrimitiveDistanceFunctor::template eval<_Scalar>( points[pid], *it2 );
+                        // store if inside scale
+                        if ( dist < params.scale )
+                        {
+                            adopter_gids[pid].push_back( std::pair<int,int>(it->first,lid1) );
+                        } //...if dist
+
+                    } //...for prims_in_patch
+                } //...for patches
+            } //...for points
+        } //...adopters
+
+        // reassign points
+        if ( iteration == 0 ) //
+        {
+            for ( std::deque<int>::iterator points_it = orphan_pids.begin(); points_it != orphan_pids.end(); /*nothing*/ )
+            {
+                // store point id
+                const int pid = *points_it;
+                std::cout << "[" << __func__ << "]: " << "point " << pid << " have " << adopter_gids[pid].size() << " adopters" << std::endl;
+
+                if ( !adopter_gids[pid].size() ) std::cerr << "[" << __func__ << "]: " << "point " << pid << " seems to be an outlier...no points could adopt it..." << std::endl;
+
+                // if point has only one possible reassignment - do assign it
+                if ( adopter_gids[pid].size() == 1 )
+                {
+                    points[pid].setTag( _PointPrimitiveT::GID, adopter_gids[pid][0].first );
+                    points_it = orphan_pids.erase( points_it ); // returns iterator to next element
+                    ++change;
+                }
+                else
+                {
+                    ++points_it; // increment only, if no erase has been done
+                }
+            }
+        }
+        else
+        {
+            int closest_gid = -3; // gid of point, that is closest to an orphan, termination crit
+            do
+            {
+                // reset
+                closest_gid = -3;
+                _Scalar                   closest_distance  = std::numeric_limits<_Scalar>::max();
+                std::deque<int>::iterator closest_orphan    = orphan_pids.begin();
+
+                for ( std::deque<int>::iterator points_it = orphan_pids.begin(); points_it != orphan_pids.end(); ++points_it )
+                {
+                    // store point id
+                    const int pid = *points_it; // orphan_pid
+
+                    // look for the closest assigned point from possible adopters
+                    for ( int pid2 = 0; pid2 != points.size(); ++pid2 )
+                    {
+                        // rule out itself
+                        if ( pid2 == pid )                                     continue;
+                        // rule out other orphans
+                        if ( points[pid2].getTag(_PointPrimitiveT::GID) < 0 )  continue;
+
+                        _Scalar dist = (points[pid].template pos() - points[pid2].template pos()).norm();
+                        // store if inside scale
+                        if ( dist < closest_distance )
+                        {
+                            closest_distance = dist;
+                            closest_gid      = points[pid2].getTag( _PointPrimitiveT::GID );
+                            closest_orphan   = points_it;
+                        } //...if dist <
+                    } //...for cloud
+                } //...for all orphans
+
+                // check, if any luck
+                if ( closest_gid >= 0 )
+                {
+                    points[ *closest_orphan ].setTag( _PointPrimitiveT::GID, closest_gid );
+                    orphan_pids.erase( closest_orphan );
+                    ++change;
+                }
+            } while ( (closest_gid >= 0) && orphan_pids.size() );
+
+        } //...if iteration != 0
+
+        ++iteration;
+    } while ( change ); // stop, if no new points were reassigned
 
     io::writeAssociations<_PointPrimitiveT>( points, "test_assoc.csv" );
 
     return EXIT_SUCCESS;
-}//...Merging::merge()
+}//...Merging::mergeCli()
 
 } //...namespace GF2
 
