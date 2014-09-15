839c89eefee7821ab20541715341204eb0ebd961
Nicolas Mellado <nmellado0@gmail.com>
2014-09-15 16:01:39 +0100

    Now recompute the GT PDF from the GT histogram and display behind it

--- evaluation/compareDistribution.py
+++ evaluation/compareDistribution.py
@@ -3,6 +3,7 @@ import packages.primitive as primitive
 import packages.io
 import argparse
 from matplotlib import pyplot as plt
+import matplotlib.mlab as mlab
 import numpy as np
 
 parser = argparse.ArgumentParser(description='Compare ground truth noise distribution (continuous generator and generated samples) and the result of the optimisation.')
@@ -33,7 +34,7 @@ gtassign = packages.io.readPointAssignementFromFiles(gtassignfile)
 
 # Generate UI
 fig, ax1 = plt.subplots()
-ax1.set_ylabel('Distribution', color='g')
+ax1.set_ylabel('PDF')
 
 
 # Generate noise kernel PDF curves
@@ -41,20 +42,28 @@ x = np.linspace(rangeMin, rangeMax, nbBins)
 
 for k in project.kernels:
     pdf = k.getPDF(x)
-    ax1.plot(x, pdf, color='g')
-    ax1.fill_between(x, pdf, 0, color='g' )
-
+    ax1.plot(x, pdf, color='#dbb971', linestyle='--')
+    
 
 # Compute the distance between each point and its assigned primitive
-d = [ [x.distanceTo(cloud[a[0]]) for x in gtlines if x.uid == a[1]][0] for a in gtassign]
+d = [ [primVar.distanceTo(cloud[a[0]]) for primVar in gtlines if primVar.uid == a[1]][0] for a in gtassign]
 
 # Generate histogram and display it
-hist, bins = np.histogram(d, nbBins/2, (rangeMin, rangeMax))
+hist, bins = np.histogram(d, nbBins/5, (rangeMin, rangeMax))
 width = 0.7 * (bins[1] - bins[0])
 center = (bins[:-1] + bins[1:]) / 2
 
 ax2 = ax1.twinx()
-ax2.bar(center, hist, align='center', width=width)
+ax2.set_ylabel('Noise')
+ax2.bar(center, hist, align='center', color='#dbb971', width=width, edgecolor = "none")
+
+# Compute PDF of the gt histogram and display
+mean = np.mean(d)
+variance = np.var(d)
+sigma = np.sqrt(variance)
+mpdf = mlab.normpdf(x,mean,sigma)
+ax1.plot(x, mpdf, linewidth=3, color='#dbb971')
+ax1.fill_between(x, mpdf, 0, color='#eddebc' )
 
 
 
