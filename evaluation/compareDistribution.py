63558f0fb8ffd41e8797d6953ede63c1b9215933
Nicolas Mellado <nmellado0@gmail.com>
2014-09-15 15:40:30 +0100

    Now load point cloud and ground truth assignement Compute and display the distance between primitives and points as histogram

--- evaluation/compareDistribution.py
+++ evaluation/compareDistribution.py
@@ -1,5 +1,6 @@
 import packages.project as project
 import packages.primitive as primitive
+import packages.io
 import argparse
 from matplotlib import pyplot as plt
 import numpy as np
@@ -9,23 +10,57 @@ parser.add_argument('projectdir')
 
 args = parser.parse_args()
 
+nbBins   = 200
+rangeMin = -0.2
+rangeMax =  0.2
+
 projectdir = args.projectdir
 if projectdir[-1] == '/':
     projectdir = projectdir[:-1]
 
-projectname = projectdir.split('/')[-1]
-projectfile = projectdir+'/gt/'+projectname+'.prj'
-gtlinesfile = projectdir+'/gt/primitives.csv'
+projectname  = projectdir.split('/')[-1]
+projectfile  = projectdir+'/gt/'+projectname+'.prj'
+gtlinesfile  = projectdir+'/gt/primitives.csv'
+gtassignfile = projectdir+'/gt/points_primitives.csv'
+cloudfile    = projectdir+'/cloud.ply'
 
 print 'Processing project ', projectname
 
-project = project.PyProject(projectfile)
-gtlines = primitive.readPrimitivesFromFile(gtlinesfile)
+project  = project.PyProject(projectfile)
+cloud    = packages.io.readPointCloudFromPly(cloudfile)
+gtlines  = primitive.readPrimitivesFromFile(gtlinesfile)
+gtassign = packages.io.readPointAssignementFromFiles(gtassignfile)
 
-x = np.linspace(-0.2, 0.2, 200)
+# Generate UI
+fig, ax1 = plt.subplots()
+ax1.set_ylabel('Distribution', color='g')
+
+
+# Generate noise kernel PDF curves
+x = np.linspace(rangeMin, rangeMax, nbBins)
 
 for k in project.kernels:
-    plt.plot(x, k.getPDF(x))
+    pdf = k.getPDF(x)
+    ax1.plot(x, pdf, color='g')
+    ax1.fill_between(x, pdf, 0, color='g' )
+
+
+# Compute the distance between each point and its assigned primitive
+d = [ [x.distanceTo(cloud[a[0]]) for x in gtlines if x.uid == a[1]][0] for a in gtassign]
+
+# Generate histogram and display it
+hist, bins = np.histogram(d, nbBins/2, (rangeMin, rangeMax))
+width = 0.7 * (bins[1] - bins[0])
+center = (bins[:-1] + bins[1:]) / 2
+
+ax2 = ax1.twinx()
+ax2.bar(center, hist, align='center', width=width)
+
+
+
+
+
+#plt.plot(hist[1], hist[0])
 
 
 plt.show()
