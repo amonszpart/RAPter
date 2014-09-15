f592bc5ab5661228eab732a0da1e366e7c8f6258
Nicolas Mellado <nmellado0@gmail.com>
2014-09-15 16:17:30 +0100

    Clean code, put UI generation in dedicated function

--- evaluation/compareDistribution.py
+++ evaluation/compareDistribution.py
@@ -6,14 +6,56 @@ from matplotlib import pyplot as plt
 import matplotlib.mlab as mlab
 import numpy as np
 
+################################################################################
+## Visualization parameters
+nbBins   = 200
+rangeMin = -0.2
+rangeMax =  0.2
+
+
+
+################################################################################
+## UI Generation
+def setupUI(primArray, assignArray, title):
+    # Generate UI
+    fig, ax1 = plt.subplots()
+    fig.canvas.set_window_title(title)
+    ax1.set_ylabel('PDF')
+
+    for k in project.kernels:
+        pdf = k.getPDF(x)
+        ax1.plot(x, pdf, color='#dbb971', linestyle='--')
+        
+
+    # Compute the distance between each point and its assigned primitive
+    d = [ [primVar.distanceTo(cloud[a[0]]) for primVar in primArray if primVar.uid == a[1]][0] for a in assignArray]
+
+    # Generate histogram and display it
+    hist, bins = np.histogram(d, nbBins/5, (rangeMin, rangeMax))
+    width = 0.7 * (bins[1] - bins[0])
+    center = (bins[:-1] + bins[1:]) / 2
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('Noise')
+    ax2.bar(center, hist, align='center', color='#dbb971', width=width, edgecolor = "none")
+
+    # Compute PDF of the gt histogram and display
+    mean = np.mean(d)
+    variance = np.var(d)
+    sigma = np.sqrt(variance)
+    mpdf = mlab.normpdf(x,mean,sigma)
+    ax1.plot(x, mpdf, linewidth=3, color='#dbb971')
+    ax1.fill_between(x, mpdf, 0, color='#eddebc' )
+
+
+
+
+################################################################################
+## Command line parsing
 parser = argparse.ArgumentParser(description='Compare ground truth noise distribution (continuous generator and generated samples) and the result of the optimisation.')
 parser.add_argument('projectdir')
 
 args = parser.parse_args()
-
-nbBins   = 200
-rangeMin = -0.2
-rangeMax =  0.2
 
 projectdir = args.projectdir
 if projectdir[-1] == '/':
@@ -27,49 +69,23 @@ cloudfile    = projectdir+'/cloud.ply'
 
 print 'Processing project ', projectname
 
+
+
+
+################################################################################
+## Reading input files
 project  = project.PyProject(projectfile)
 cloud    = packages.io.readPointCloudFromPly(cloudfile)
 gtlines  = primitive.readPrimitivesFromFile(gtlinesfile)
 gtassign = packages.io.readPointAssignementFromFiles(gtassignfile)
 
-# Generate UI
-fig, ax1 = plt.subplots()
-ax1.set_ylabel('PDF')
 
 
-# Generate noise kernel PDF curves
+################################################################################
+## Generate UI
 x = np.linspace(rangeMin, rangeMax, nbBins)
 
-for k in project.kernels:
-    pdf = k.getPDF(x)
-    ax1.plot(x, pdf, color='#dbb971', linestyle='--')
-    
-
-# Compute the distance between each point and its assigned primitive
-d = [ [primVar.distanceTo(cloud[a[0]]) for primVar in gtlines if primVar.uid == a[1]][0] for a in gtassign]
-
-# Generate histogram and display it
-hist, bins = np.histogram(d, nbBins/5, (rangeMin, rangeMax))
-width = 0.7 * (bins[1] - bins[0])
-center = (bins[:-1] + bins[1:]) / 2
-
-ax2 = ax1.twinx()
-ax2.set_ylabel('Noise')
-ax2.bar(center, hist, align='center', color='#dbb971', width=width, edgecolor = "none")
-
-# Compute PDF of the gt histogram and display
-mean = np.mean(d)
-variance = np.var(d)
-sigma = np.sqrt(variance)
-mpdf = mlab.normpdf(x,mean,sigma)
-ax1.plot(x, mpdf, linewidth=3, color='#dbb971')
-ax1.fill_between(x, mpdf, 0, color='#eddebc' )
-
-
-
-
-
-#plt.plot(hist[1], hist[0])
-
+setupUI(gtlines, gtassign, "Ground truth")
+setupUI(gtlines, gtassign, "Estimation")
 
 plt.show()
