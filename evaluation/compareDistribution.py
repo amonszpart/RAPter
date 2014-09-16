718bfa23b6dedfd63777529554872f934b284ee9
Nicolas Mellado <nmellado0@gmail.com>
2014-09-16 09:17:06 +0100

    Add Kolmogorov-Smirnov test

--- evaluation/compareDistribution.py
+++ evaluation/compareDistribution.py
@@ -5,48 +5,66 @@ import argparse
 from matplotlib import pyplot as plt
 import matplotlib.mlab as mlab
 import numpy as np
+from scipy.stats import norm,kstest,skewtest,kurtosistest,normaltest
+from scipy.integrate import quad
 
 ################################################################################
 ## Visualization parameters
 nbBins   = 200
-rangeMin = -0.2
-rangeMax =  0.2
+rangeMin = -0.05
+rangeMax =  0.05
 
 
 
 ################################################################################
 ## UI Generation
-def setupUI(primArray, assignArray, title):
+def setupHistogramUI(distribution, title, c1, c2, pdf=None):
     # Generate UI
     fig, ax1 = plt.subplots()
     fig.canvas.set_window_title(title)
     ax1.set_ylabel('PDF')
 
-    for k in project.kernels:
-        pdf = k.getPDF(x)
-        ax1.plot(x, pdf, color='#dbb971', linestyle='--')
+    if pdf != None:
+        ax1.plot(x, pdf, color=c1, linestyle='--')
         
-
-    # Compute the distance between each point and its assigned primitive
-    d = [ [primVar.distanceTo(cloud[a[0]]) for primVar in primArray if primVar.uid == a[1]][0] for a in assignArray]
-
     # Generate histogram and display it
-    hist, bins = np.histogram(d, nbBins/5, (rangeMin, rangeMax))
+    hist, bins = np.histogram(distribution, nbBins/4, (rangeMin, rangeMax), normed=True)
     width = 0.7 * (bins[1] - bins[0])
     center = (bins[:-1] + bins[1:]) / 2
 
     ax2 = ax1.twinx()
     ax2.set_ylabel('Noise')
-    ax2.bar(center, hist, align='center', color='#dbb971', width=width, edgecolor = "none")
+    ax2.bar(center, hist, align='center', color=c1, width=width, edgecolor = "none")
 
     # Compute PDF of the gt histogram and display
-    mean = np.mean(d)
-    variance = np.var(d)
+    mean = np.mean(distribution)
+    variance = np.var(distribution)
     sigma = np.sqrt(variance)
     mpdf = mlab.normpdf(x,mean,sigma)
-    ax1.plot(x, mpdf, linewidth=3, color='#dbb971')
-    ax1.fill_between(x, mpdf, 0, color='#eddebc' )
+    ax1.plot(x, mpdf, linewidth=3, color=c1)
+    ax1.fill_between(x, mpdf, 0, color=c2 )
+    
 
+def setupComparisonUI(distrib1, title1, c1, 
+                      distrib2, title2, c2,
+                      kernel = None, c3 = None):
+    fig, ax1 = plt.subplots()
+    fig.canvas.set_window_title('CDFs')
+    
+    hist1, bins = np.histogram(distrib1, nbBins, (rangeMin, rangeMax), density=True)    
+    hist2, bins = np.histogram(distrib2, nbBins, (rangeMin, rangeMax), density=True)
+    
+    ax1.plot(x, np.cumsum(hist1)/np.sum(hist1), color=c1)
+    ax1.plot(x, np.cumsum(hist2)/np.sum(hist2), color=c2)
+    
+    if kernel != None:
+      ax1.plot(x, kernel.cdf(x), color=c3, linestyle='--')
+      
+      print 'Analyzing ',title1,': ',kstest(distrib1, kernel.cdf, mode='asymp')
+      print 'Analyzing ',title2,': ',kstest(distrib2, kernel.cdf, mode='asymp')
+    
+    
+    
 
 
 
@@ -67,6 +85,9 @@ gtlinesfile  = projectdir+'/gt/primitives.csv'
 gtassignfile = projectdir+'/gt/points_primitives.csv'
 cloudfile    = projectdir+'/cloud.ply'
 
+linesfile_it1  = projectdir+'/primitives_merged_it1.csv'
+assignfile_it1 = projectdir+'/points_primitives_it1.csv'
+
 print 'Processing project ', projectname
 
 
@@ -79,13 +100,35 @@ cloud    = packages.io.readPointCloudFromPly(cloudfile)
 gtlines  = primitive.readPrimitivesFromFile(gtlinesfile)
 gtassign = packages.io.readPointAssignementFromFiles(gtassignfile)
 
+lines_it1  = primitive.readPrimitivesFromFile(linesfile_it1)
+assign_it1 = packages.io.readPointAssignementFromFiles(assignfile_it1)
+
+
+
+################################################################################
+## Process data
+
+# Compute the distance between each point and its assigned primitive
+gtDistrib   = [ [primVar.distanceTo(cloud[a[0]]) for primVar in gtlines if primVar.uid == a[1]][0] for a in gtassign]
+distrib_it1 = [ [primVar.distanceTo(cloud[a[0]]) for primVar in lines_it1 if primVar.uid == a[1]][0] for a in assign_it1]
 
 
 ################################################################################
 ## Generate UI
 x = np.linspace(rangeMin, rangeMax, nbBins)
 
-setupUI(gtlines, gtassign, "Ground truth")
-setupUI(gtlines, gtassign, "Estimation")
+pdf = project.kernels[0]._pdf(x)
+
+setupHistogramUI(gtDistrib, "Ground truth", '#dbb971', '#eddebc', pdf)
+setupHistogramUI(distrib_it1, "Estimation", '#71b2db', '#b4d9f1', pdf)
+setupComparisonUI(gtDistrib, "Ground truth", '#dbb971', 
+                  distrib_it1, "Estimation", '#71b2db',
+                  project.kernels[0], '#dbb971')
+ 
+#print 'normal skewtest teststat = %6.3f pvalue = %6.4f' % skewtest(gtDistrib)
+#print 'normal kurtosistest teststat = %6.3f pvalue = %6.4f' % kurtosistest(gtDistrib)
+#print 'normal teststat = %6.3f pvalue = %6.4f' % normaltest(gtDistrib)
+#skewtest(gtDistrib)                 
+#print kstest(gtDistrib, project.kernels[0].cdf, mode='asymp')
 
 plt.show()
