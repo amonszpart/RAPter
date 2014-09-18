be60862eaa5d94fc1ed36ee0fbce37cb44af55bb
Aron Monszpart <a.monszpart@cs.ucl.ac.uk>
2014-09-18 23:38:40 +0100

    - very initial visal comparison script showPearl.sh in scripts

---
+++ globOpt/scripts/showPearl.sh
@@ -0,0 +1,2 @@
+../globOptVis --show --scale 0.05 --pop-limit 0 -p primitives_merged_it2.csv -a points_primitives_it2.csv --title globOpt --use-tags --no-clusters --no-pop &
+../globOptVis --show --scale 0.05 --pop-limit 0 -p primitives.pearl.csv -a points_primitives.pearl.csv --title Pearl --use-tags --no-clusters --no-pop &
