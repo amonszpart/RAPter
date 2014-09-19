5b271b05747b608063a7c5f46ad4169c66759bba
Aron Monszpart <a.monszpart@cs.ucl.ac.uk>
2014-09-19 23:52:46 +0100

    - created corresp executable

--- globOpt/src/correspondence.cpp
+++ globOpt/src/correspondence.cpp
@@ -4,6 +4,7 @@
 // ________________correspondance___________________
 
 #include "globfit2/io/io.h"         // readPrimitives()
+#include "globfit2/util/diskUtil.hpp" // saveBackup()
 
 namespace GF2 {
 namespace correspondence
@@ -24,8 +25,8 @@ namespace correspondence
     int correspCli( int argc, char**argv )
     {
         typedef std::map<int, _InnerPrimitiveContainerT> PrimitiveMapT;
-        enum { PNT_GID    = _PointPrimitiveT::GID
-             , GT_PNT_GID = _PointPrimitiveT::USER_ID1
+        enum { PNT_GID_B    = _PointPrimitiveT::GID
+             , PNT_GID_A    = _PointPrimitiveT::USER_ID1
              };
 
         typedef std::pair< int   , int    > GidLid;   //!< Uniquely identifies an entry in PrimitiveMapT.
@@ -34,59 +35,71 @@ namespace correspondence
         int err = EXIT_SUCCESS;
 
         // print usage
-        if ( GF2::console::find_switch(argc,argv,"-h") || GF2::console::find_switch(argc,argv,"--help") )
+        if ( GF2::console::find_switch(argc,argv,"-h")
+             || GF2::console::find_switch(argc,argv,"--help")
+             || (argc != 6) )
         {
             std::cout << "Usage: "
-                      << argv[0] << " --corresp"
-                      << " --gt gt_prims.csv \n"
-                      << " --gta gt_points_primitives.csv\n"
-                      << " --p prims.csv\n"
-                      << " --pa points_primitives.csv\n"
-                      << " --cloud cloud.ply\n"
+                      << argv[0] << "\n"
+                      << " primsA.csv \n"
+                      << " points_primitivesA.csv\n"
+                      << " primsB.csv \n"
+                      << " points_primitivesB.csv\n"
+                      << " cloud.ply\n"
                       ;
             return err;
         } //...print usage
 
         // parse input
-        std::string gt_path,
-                    prims_path,
+        std::string prims_pathA,
+                    prims_pathB,
                     cloud_path = "./cloud.ply",
-                    assoc_path,
-                    gt_assoc_path;
+                    assoc_pathB,
+                    assoc_pathA;
         {
-            if ( GF2::console::parse_argument(argc,argv,"--gt",gt_path) < 0 )
+            //if ( GF2::console::parse_argument(argc,argv,"--gt",gt_path) < 0 )
+            prims_pathA = std::string( argv[1] );
+            if ( !boost::filesystem::exists(prims_pathA) )
             {
-                std::cerr << "[" << __func__ << "]: " << "need --gt gt_prims.csv to work" << std::endl;
+                std::cerr << "[" << __func__ << "]: " << "need prims_pathA" << prims_pathA << " to exist!" << std::endl;
                 return EXIT_FAILURE;
             }
 
-            if ( GF2::console::parse_argument(argc,argv,"--p",prims_path) < 0 )
+            // if ( GF2::console::parse_argument(argc,argv,"--gta",gt_assoc_path) < 0 )
+            assoc_pathA = std::string( argv[2] );
+            if ( !boost::filesystem::exists(assoc_pathA) )
             {
-                std::cerr << "[" << __func__ << "]: " << "need --p gt_prims.csv to work" << std::endl;
+                std::cerr << "[" << __func__ << "]: " << "need assoc_pathA" << assoc_pathA << " to exist!" << std::endl;
                 return EXIT_FAILURE;
             }
 
-            if ( GF2::console::parse_argument(argc,argv,"--gta",gt_assoc_path) < 0 )
+            //if ( GF2::console::parse_argument(argc,argv,"--p",prims_path) < 0 )
+            prims_pathB = std::string( argv[3] );
+            if ( !boost::filesystem::exists(prims_pathB) )
             {
-                std::cerr << "[" << __func__ << "]: " << "need --gta gt_points_primitives.csv to work" << std::endl;
+                std::cerr << "[" << __func__ << "]: " << "need prims_pathB" << prims_pathB << "to exist!" << std::endl;
                 return EXIT_FAILURE;
             }
 
-            if ( GF2::console::parse_argument(argc,argv,"--pa", assoc_path) < 0 )
+            //if ( GF2::console::parse_argument(argc,argv,"--pa", assoc_path) < 0 )
+            assoc_pathB = std::string( argv[4] );
+            if ( !boost::filesystem::exists(assoc_pathB) )
             {
-                std::cerr << "[" << __func__ << "]: " << "need --pa points_primitives.csv to work" << std::endl;
+                std::cerr << "[" << __func__ << "]: " << "need assoc_pathB " << assoc_pathB << " to exist!" << std::endl;
                 return EXIT_FAILURE;
             }
 
-            if ( (GF2::console::parse_argument(argc,argv,"--cloud",cloud_path) < 0) && !boost::filesystem::exists(cloud_path) )
+            //if ( (GF2::console::parse_argument(argc,argv,"--cloud",cloud_path) < 0) && !boost::filesystem::exists(cloud_path) )
+            cloud_path = std::string( argv[5] );
+            if ( !boost::filesystem::exists(cloud_path) )
             {
-                std::cerr << "[" << __func__ << "]: " << "need --cloud cloud.ply to work" << std::endl;
+                std::cerr << "[" << __func__ << "]: " << "need cloud_path " << cloud_path << "to exist!" << std::endl;
                 return EXIT_FAILURE;
             }
         } //...parse input
 
         _PointContainerT     points; // need two versions to hold the associations...:-S
-        PrimitiveMapT        gt_prims_map, prims_map;
+        PrimitiveMapT        prims_mapA, prims_mapB;
         // read input
         {
             // Read points
@@ -100,36 +113,36 @@ namespace correspondence
 
             // read GT associations
             {
-                io::readAssociations( points_primitives, gt_assoc_path, NULL );
+                io::readAssociations( points_primitives, assoc_pathA, NULL );
                 for ( size_t i = 0; i != points.size(); ++i )
                 {
                     // store association in point
-                    points[i].setTag( GT_PNT_GID, points_primitives[i].first );
+                    points[i].setTag( PNT_GID_A, points_primitives[i].first );
                 }
             }
 
             // read associations
             {
-                io::readAssociations( points_primitives, assoc_path, NULL );
+                io::readAssociations( points_primitives, assoc_pathB, NULL );
                 for ( size_t i = 0; i != points.size(); ++i )
                 {
                     // store association in point
-                    points[i].setTag( PNT_GID, points_primitives[i].first );
+                    points[i].setTag( PNT_GID_B, points_primitives[i].first );
                 }
             }
 
             // read primitives
-            _PrimitiveContainerT gt_primitives, primitives; // unused, so local scope
+            _PrimitiveContainerT primitivesA, primitivesB; // unused, so local scope
             {
                 // GT
-                std::cout << "[" << __func__ << "]: " << "reading GT primitives from " << gt_path << "...";
-                io::readPrimitives<_PrimitiveT, _InnerPrimitiveContainerT>( gt_primitives, gt_path, &gt_prims_map );
-                std::cout << "reading GT primitives ok (#: " << gt_prims_map.size() << ")\n";
+                std::cout << "[" << __func__ << "]: " << "reading primitivesA from " << prims_pathA << "...";
+                io::readPrimitives<_PrimitiveT, _InnerPrimitiveContainerT>( primitivesA, prims_pathA, &prims_mapA );
+                std::cout << "reading primitivesA ok (#: " << prims_mapA.size() << ")\n";
 
                 // solution
-                std::cout << "[" << __func__ << "]: " << "reading primitives from " << prims_path << "...";
-                io::readPrimitives<_PrimitiveT, _InnerPrimitiveContainerT>( primitives, prims_path, &prims_map );
-                std::cout << "reading primitives ok (#: " << prims_map.size() << ")\n";
+                std::cout << "[" << __func__ << "]: " << "reading primitivesB from " << prims_pathB << "...";
+                io::readPrimitives<_PrimitiveT, _InnerPrimitiveContainerT>( primitivesB, prims_pathB, &prims_mapB );
+                std::cout << "reading primitivesB ok (#: " << prims_mapB.size() << ")\n";
             } //...read primitives
         }
 
@@ -144,22 +157,22 @@ namespace correspondence
             CostMap costs;
 
             // calc distances
-            int gid, gid_gt, lid, lid_gt;
-            for ( outer_const_iterator outer_it0 = prims_map.begin(); outer_it0 != prims_map.end(); ++outer_it0 )
+            int gidA, gidB, lidA, lidB;
+            for ( outer_const_iterator outer_it0 = prims_mapA.begin(); outer_it0 != prims_mapA.end(); ++outer_it0 )
             {
-                gid = (*outer_it0).first;
-                lid = 0;
-                for ( inner_const_iterator inner_it0 = (*outer_it0).second.begin(); inner_it0 != (*outer_it0).second.end(); ++inner_it0, ++lid )
+                gidA = (*outer_it0).first;
+                lidA = 0;
+                for ( inner_const_iterator inner_it0 = (*outer_it0).second.begin(); inner_it0 != (*outer_it0).second.end(); ++inner_it0, ++lidA )
                 {
-                    for ( outer_const_iterator outer_it1 = gt_prims_map.begin(); outer_it1 != gt_prims_map.end(); ++outer_it1 )
+                    for ( outer_const_iterator outer_it1 = prims_mapB.begin(); outer_it1 != prims_mapB.end(); ++outer_it1 )
                     {
-                        gid_gt = (*outer_it1).first;
-                        lid_gt = 0;
-                        for ( inner_const_iterator inner_it1 = (*outer_it1).second.begin(); inner_it1 != (*outer_it1).second.end(); ++inner_it1, ++lid_gt )
+                        gidB = (*outer_it1).first;
+                        lidB = 0;
+                        for ( inner_const_iterator inner_it1 = (*outer_it1).second.begin(); inner_it1 != (*outer_it1).second.end(); ++inner_it1, ++lidB )
                         {
-                            std::cout << "checking " << gid << "." << lid << " vs " << gid_gt << "." << lid_gt;
-                            costs[ CostKey(GidLid(gid,lid),GidLid(gid_gt,lid_gt)) ] = estimateDistance<float>( *inner_it0, *inner_it1, points, PNT_GID, GT_PNT_GID );
-                            std::cout << ": " << costs[ CostKey(GidLid(gid,lid),GidLid(gid_gt,lid_gt)) ] << std::endl;
+                            std::cout << "checking " << gidA << "." << lidA << " vs " << gidB << "." << lidB;
+                            costs[ CostKey(GidLid(gidA,lidA),GidLid(gidB,lidB)) ] = estimateDistance<float>( *inner_it0, *inner_it1, points, PNT_GID_A, PNT_GID_B );
+                            std::cout << ": " << costs[ CostKey(GidLid(gidA,lidA),GidLid(gidB,lidB)) ] << std::endl;
                         } //...for gt_prims
                     } //...for gt_prims_map
                 } //...for prims
@@ -179,45 +192,72 @@ namespace correspondence
                 std::sort( cost_list.begin(), cost_list.end() );
 
                 // select
-                std::set<GidLid> taken_prims, taken_gts;
+                std::set<GidLid> taken_primsA, taken_primsB;
                 for ( int i = 0; i < cost_list.size(); ++i )
                 {
-                    CostKey const& costKey    = cost_list[i].second;
-                    GidLid  const& primGidLid = costKey.first;
-                    GidLid  const& gtGidLid   = costKey.second;
-                    std::cout << "cost_list[" << i << "]: " << cost_list[i].first << " for "
-                              << primGidLid.first << "." << primGidLid.second << " - "
-                              << gtGidLid.first << "." << gtGidLid.second << std::endl;
+                    CostKey const& costKey   = cost_list[i].second;
+                    GidLid  const& gidLidA   = costKey.first;
+                    GidLid  const& gidLidB   = costKey.second;
+                    //std::cout << "cost_list[" << i << "]: " << cost_list[i].first << " for "
+                    //          << gidLidA.first << "." << gidLidA.second << " - "
+                    //          << gidLidB.first << "." << gidLidB.second << std::endl;
 
-                    if ( (taken_prims.find(primGidLid) == taken_prims.end()) && (taken_gts.find(gtGidLid) == taken_gts.end()) )
+                    // if both untaken
+                    if ( (taken_primsA.find(gidLidA) == taken_primsA.end()) && (taken_primsB.find(gidLidB) == taken_primsB.end()) )
                     {
                         std::cout << "chose " << cost_list[i].first << " for "
-                                  << primGidLid.first << "." << primGidLid.second << " - "
-                                  << gtGidLid.first << "." << gtGidLid.second << std::endl;
+                                  << gidLidA.first << "." << gidLidA.second << " - "
+                                  << gidLidB.first << "." << gidLidB.second << std::endl;
 
-                        taken_prims.insert( primGidLid );
-                        taken_gts  .insert( gtGidLid   );
+                        taken_primsA.insert( gidLidA );
+                        taken_primsB.insert( gidLidB   );
 
-                        if ( corresps.find(primGidLid) != corresps.end() )
+                        if ( corresps.find(gidLidA) != corresps.end() )
                             std::cerr << "[" << __func__ << "]: " << "duplicate choice...should not happen!" << std::endl;
 
-                        corresps[ primGidLid ] = gtGidLid;
-                    }
-                }
-            }
+                        corresps[ gidLidA ] = gidLidB;
+                    } //...if both untaken
+                } //...for each cost entry
+            } //...corresps
 
             // print
             {
+                std::string corresp_path = "./corresp.csv";
+                GF2::util::saveBackup( corresp_path );
+
+                std::ofstream corresp_f( corresp_path );
+                if ( !corresp_f.is_open() )
+                {
+                    std::cerr << "[" << __func__ << "]: " << "could not open " << corresp_path << std::endl;
+                    return EXIT_FAILURE;
+                }
+
+                // debug
+                PrimitiveMapT subs;
+
+                corresp_f << "# corresp between\n# " << prims_pathA << "," << prims_pathB << std::endl;
                 for ( CorrespT::const_iterator it = corresps.begin(); it != corresps.end(); ++it )
                 {
-                    GidLid const& primGidLid = (*it).first;
-                    GidLid const& gtGidLid = (*it).second;
-                    std::cout << "prims[" << primGidLid.first << "][" << primGidLid.second << "]: " << prims_map[primGidLid.first][primGidLid.second].toString()
+                    GidLid const& gidLidA = (*it).first;
+                    GidLid const& gidLidB = (*it).second;
+
+                    std::cout << "prims[" << gidLidA.first << "][" << gidLidA.second << "]: " << prims_mapA[gidLidA.first][gidLidA.second].toString()
                               << " <--> "
-                              << "gt[" << gtGidLid.first << "][" << gtGidLid.second << "]: " << gt_prims_map[gtGidLid.first][gtGidLid.second].toString()
-                              << " with cost " << costs[ CostKey(primGidLid,gtGidLid) ]
+                              << "gt[" << gidLidB.first << "][" << gidLidB.second << "]: " << prims_mapB[gidLidB.first][gidLidB.second].toString()
+                              << " with cost " << costs[ CostKey(gidLidA,gidLidB) ]
                               << std::endl;
+
+                    corresp_f << gidLidA.first  << ","
+                              << gidLidA.second << ","
+                              << gidLidB.first  << ","
+                              << gidLidB.second << "\n";
+
+                    containers::add( subs, gidLidA.first, prims_mapB[gidLidB.first][gidLidB.second] );
                 }
+
+                corresp_f.close();
+
+                GF2::io::savePrimitives<_PrimitiveT,typename _InnerPrimitiveContainerT::const_iterator>( subs, "subs.csv" );
             }
         } //...work
 
@@ -227,9 +267,13 @@ namespace correspondence
 } //...namespace correspondance
 } //...namespace GF2
 
-int corresp( int argc, char** argv )
+int main( int argc, char** argv )
 {
-    if ( GF2::console::find_switch(argc,argv,"--corresp") )
+    if ( GF2::console::find_switch(argc,argv,"--corresp3D") )
+    {
+        std::cerr << "corresp3D unimplemented" << std::endl;
+    }
+    else
     {
         return GF2::correspondence::correspCli< GF2::_2d::PrimitiveT
                                               , GF2::_2d::InnerPrimitiveContainerT
