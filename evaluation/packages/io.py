6039fd841bdddaa8fc35dcf11c2e1c71d95da66d
Nicolas Mellado <nmellado0@gmail.com>
2014-09-22 16:05:33 +0100

    Add new method to read correspondances files

--- evaluation/packages/io.py
+++ evaluation/packages/io.py
@@ -28,5 +28,38 @@ def readPointAssignementFromFiles(path):
     
     return assignement
     
+def readPrimitiveCorrespondancesFromFiles(path, primset1, primset2):
+    f = open(path, 'r')
+    
+    corresp    = {}
+    correspUid = {}
+    
+    for line in f:
+        if line[0] != '#':
+            fline = np.int16(line.split(',')[0:6])
+            
+            p1 = None
+            p2 = None
+            
+            for p in primset1:
+                if p.uid == fline[0] and p.did == fline[2]:
+                    p1 = p
+                    break
+            
+            for p in primset2:
+                if p.uid == fline[3] and p.did == fline[5]:
+                    p2 = p
+                    break
+            
+            if (p1 != None and p2 != None):
+                corresp[p1] = p2
+                correspUid[p1.uid] = p2.uid
+            else: 
+                print "Cannot find ",fline
+            
+    f
+    
+    return corresp, correspUid
+    
     
     
