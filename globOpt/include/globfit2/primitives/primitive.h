08c1dbcabc99b7ca313bbbcc7d0f7ef30fbd39f8
Aron Monszpart <a.monszpart@cs.ucl.ac.uk>
2014-09-22 14:50:49 +0100

    primitive.h     - added STATUS_VALUES::UNSET to status values to make it more clear

--- globOpt/include/globfit2/primitives/primitive.h
+++ globOpt/include/globfit2/primitives/primitive.h
@@ -26,7 +26,7 @@ namespace GF2
 
             enum STATUS_VALUES { ACTIVE = 1 //!< Used to show, that this primitive is active. Also used as starting point in \ref GF2::Solver.
                                , SMALL  = 2 //!< Used to show, that this primitive is *not* active, but needs to be kept for later use, so copied to output everywhere.
-                             /*, UNSET  = -1 */ // implicit from Taggable implementation
+                               , UNSET  = ::GF2::Taggable::TAG_UNSET
                                };
             //! \brief Defines the tags (ids) that this primitive can manage using setTag and getTag functions.
             enum TAGS {
