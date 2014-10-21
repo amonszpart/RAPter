c583f139b5092c132cb738f8bcbb5f305a0204a9
Nicolas Mellado <nmellado0@gmail.com>
2014-10-21 15:05:17 +0100

    Add add function taking a functor as input to process connected primitives

--- evaluation/packages/relationGraph.py
+++ evaluation/packages/relationGraph.py
@@ -9,10 +9,12 @@ import packages.primitive as primitive
 class RelationGraph(object):
     def __init__(self,primArray, assignArray):
         self.G=nx.Graph()
+        self.indexedPrimArray = {}
         
         # First create the nodes
         for p in primArray:
             self.G.add_node(p.uid, w=0)
+            self.indexedPrimArray[p.uid] = p
         
         # Then their relations
         for idx1, p1 in enumerate(primArray):
@@ -29,6 +31,11 @@ class RelationGraph(object):
                     
         #print "Number of primitives:  ",self.G.number_of_nodes()
         #print "Number of connections: ",self.G.number_of_edges()
+        
+    # Call the functor over the primitives connected by and edge
+    def processConnectedNodes(self, functor):
+        for e in self.G.edges():
+            functor( self.indexedPrimArray[e[0]], self.indexedPrimArray[e[1]])
         
     def draw(self):
         nx.draw_spectral(self.G)
