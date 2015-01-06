96733354958df3271cff87a6ee87726d41d1c4a2
Aron Monszpart <a.monszpart@cs.ucl.ac.uk>
2015-01-06 15:14:22 +0000

    - small fixes to 2D_SAMPLE

--- globOpt/include/globfit2/ground_truth/gtCreator.h
+++ globOpt/include/globfit2/ground_truth/gtCreator.h
@@ -4,24 +4,31 @@
 #include <string>
 #include "pcl/point_cloud.h"
 #include "globfit2/my_types.h"
+#include <opencv2/core.hpp>
 
 namespace GF2
 {
 
     class GTCreator
     {
-        public:
-            typedef pcl::PointXYZRGB PointT;
+    public:
+        typedef pcl::PointXYZRGB PointT;
 
-            //static int run( pcl::PointCloud<PointT>::Ptr &cloud, std::string gt_name, int gt_nPoints, float gt_noise );
-            static int sampleImage( pcl::PointCloud<PointT>::Ptr &cloud
-                                    , std::string image_path
-                                    , int gt_nPoints
-                                    , float gt_noise
-                                    , float gt_scene_size            = 1.f
-                                    , float filter_size              = 0.075f
-                                    , Eigen::Vector3f *sensor_origin = NULL
-                                   );
+        int
+        image_2_2DCloud( pcl::PointCloud<PointT>::Ptr &cloud
+                         , cv::Mat                const& img
+                         , int                         N_samples
+                         , const float                 Z          );
+
+        //static int run( pcl::PointCloud<PointT>::Ptr &cloud, std::string gt_name, int gt_nPoints, float gt_noise );
+        static int sampleImage( pcl::PointCloud<PointT>::Ptr &cloud
+                                , std::string image_path
+                                , int gt_nPoints
+                                , float gt_noise
+                                , float gt_scene_size            = 1.f
+                , float filter_size              = 0.075f
+                , Eigen::Vector3f *sensor_origin = NULL
+                );
     };
 
 } // ns GF2
