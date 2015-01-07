f9332afe031f4d7875b8c6dd53392a46a198fc9e
Nicolas Mellado <nmellado0@gmail.com>
2015-01-07 12:00:41 +0100

    Add method to parse angle command line arguments

--- evaluation/packages/utils.py
+++ evaluation/packages/utils.py
@@ -3,3 +3,21 @@
 # Return an array with len=len(assign)
 def distanceToPrimitives(cloud, assign, primitives):
     return [ [primVar.distanceTo(cloud[a[0]]) for primVar in primitives if primVar.uid == a[1]] for a in assign]
+    
+    
+   
+import packages.orderedSet as orderedSet 
+def parseAngles(strAngle):
+    angles = orderedSet.OrderedSet()
+    angles.add(0.)
+    if len(strAngle) == 1:
+        strAngle = strAngle[0].split(',')
+
+    for genAngle in strAngle:
+        a = float(genAngle)
+        while a <= 180.:
+            angles.add(a)
+            a+= float(genAngle)
+    
+    return angles
+    
