8dd6aebd5d82e5f728e1498fda323be5bc6ba566
Nicolas Mellado <nmellado0@gmail.com>
2015-01-12 17:22:39 +0100

    Fix wrong includes

--- globOpt/include/globfit2/io/depthIo.hpp
+++ globOpt/include/globfit2/io/depthIo.hpp
@@ -3,8 +3,8 @@
 
 #include <string>
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include "Eigen/Dense"
 
 namespace GF2 {
