6840d9318a033b861be20836d73641ae60821dad
Nicolas Mellado <nmellado0@gmail.com>
2015-01-15 19:37:12 +0100

    Add instructions tor generate correct plots

---
+++ data/scenes-paper/overview-sigg/run.sh
@@ -0,0 +1,7 @@
+python ../generatePolarPrimDirections.py segments.csv  --shownormals --useInactivePrims
+python ../generatePolarPrimDirections.py patches.csv  --shownormals --useInactivePrims
+python ../generatePolarPrimDirections.py primitives_it0.bonmin.csv  --shownormals
+python ../generatePolarPrimDirections.py primitives_it1.bonmin.csv  --shownormals --invertnormals
+python ../generatePolarPrimDirections.py primitives_it2.bonmin.csv  --shownormals
+python ../generatePolarPrimDirections.py primitives_it3.bonmin.csv  --shownormals --invertnormals
+python ../generatePolarPrimDirections.py primitives_it4.bonmin.csv  --shownormals
