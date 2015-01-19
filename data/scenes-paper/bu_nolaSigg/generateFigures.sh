55ebe86a327673471203a784a85869abb5ecbb43
Nicolas Mellado <nmellado0@gmail.com>
2015-01-19 11:13:30 +0100

    UPdate commands

--- data/scenes-paper/bu_nolaSigg/generateFigures.sh
+++ data/scenes-paper/bu_nolaSigg/generateFigures.sh
@@ -58,8 +58,8 @@ meshlabserver -i schnabel_minsup500_pts.ply -o figure/schnabel_minsup500_pts.ply
 splatting figure/schnabel_minsup500_pts.ply figure/schnabel_minsup500.ply 1 0.002
 
 #distribution
-python ../normal_distr.py schnabel_minsup500_pts.ply ndistr_schnabel_minsup500.svg "Lans - Schnabel (minsup=20000)"
+python ../normal_distr.py schnabel_minsup500_pts.ply ndistr_schnabel_minsup500.svg "Lans - Schnabel (minsup=500)"
 
-../globOptVis --show3D --scale 0.02 --pop-limit 3 --title "Schnabel 20000" --angle-gens 90 --use-tags --no-clusters --statuses -1,1 --no-pop --dir-colours --no-scale --bg-colour .9,.9,.9 --no-rel -p schnabel_minsup500.primitives.csv -a schnabel_minsup500.points_primitives.csv --cloud schnabel_minsup500.cloud.ply  --perfect-angle 0.0001  --draw-mode 1 --save-poly  --no-pts 
+../globOptVis --show3D --scale 0.02 --pop-limit 3 --title "Schnabel 500" --angle-gens 90 --use-tags --no-clusters --statuses -1,1 --no-pop --dir-colours --no-scale --bg-colour .9,.9,.9 --no-rel -p schnabel_minsup500.primitives.csv -a schnabel_minsup500.points_primitives.csv --cloud schnabel_minsup500.cloud.ply  --perfect-angle 0.0001  --draw-mode 1 --save-poly  --no-pts 
 
 mv plane_mesh.ply figure/schnabel_minsup500_planes.ply
