ca2df92afc27db66be2ce6fbf64884a1cc2c5c96
Aron Monszpart <a.monszpart@cs.ucl.ac.uk>
2015-04-16 16:31:26 +0100

    - updated empFull with unambiguous point assignments

--- data/scenes-paper/bu_empFull/aronAngles.sh
+++ data/scenes-paper/bu_empFull/aronAngles.sh
@@ -4,3 +4,5 @@
 /home/bontius/workspace/globOpt/globOpt/build/Release/bin/eval --planes --assign gt.obj --cloud schnabel_minsup20000.cloud.ply -p schnabel_minsup20000.primitives.csv -a schnabel_minsup20000.points_primitives.csv --scale 0.0025 --silent
 
 /home/bontius/workspace/globOpt/globOpt/build/Release/bin/eval --planes --assign gt.obj --cloud cloud_100000.ply -p primitives.pearl.csv -a points_primitives.pearl.csv --scale 0.0025 --silent
+
+/home/bontius/workspace/globOpt/globOpt/build/Release/bin/eval --planes --assign gt.obj --cloud cloud.ply -p globfit_0.5_13/patches.sub_0.5_13.csv -a globfit_0.5_13/points_patches.sub_0.5_13.csv --scale 0.0025 --silent
